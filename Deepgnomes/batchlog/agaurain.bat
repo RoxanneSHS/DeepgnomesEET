0725aa310d1207dd241b592d31a8034945752ca4
Roxanne <cwillenbockel@aol.com>
2017-03-07 19:13:03 +0100

    First commit

---
+++ Deepgnomes/batchlog/agaurain.bat
@@ -0,0 +1,8 @@
+@echo off
+
+aurora\batchlog\tisunpack -s -o override\ag0540.tis aurora\tiz\ag0540.tiz
+aurora\batchlog\tisunpack -s -o override\ag1110.tis aurora\tiz\ag1110.tiz
+aurora\batchlog\tisunpack -s -o override\ag2020.tis aurora\tiz\ag2020.tiz
+
+:end
+::::
